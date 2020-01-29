6f0305b9b155fe465a16956449878f3f110d93fa
Keefer Taylor <keefer.t@gmail.com>
2020-01-29 13:24:17 -0500

    actual tests

--- src/test/java/io/xpring/xrpl/SignerTest.java
+++ src/test/java/io/xpring/xrpl/SignerTest.java
@@ -12,7 +12,7 @@ import rpc.v1.Amount.XRPDropsAmount;
 import rpc.v1.TransactionOuterClass.Payment;
 import rpc.v1.TransactionOuterClass.Transaction;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
 public class SignerTest {
     @Test
@@ -33,20 +33,19 @@ public class SignerTest {
                 .setSequence(sequence)
                 .setPayment(payment)
                 .build();
-//
-//        byte [] expected = {
-//                18, 0, 0, 36, 0, 0, 0, 1, 32, 27, 0, 0, 0, 0, 97, 64, 0, 0, 0, 0, 0, 3, 232, 104, 64, 0, 0, 0, 0, 0, 0,
-//                10, 115, 0, 116, 71, 48, 69, 2, 33, 0, (byte) 245, 52, 189, 33, 190, 200, 90, 121, 248, 227, 64, 199, 240, 205,
-//                (byte) 221, 62, 60, 86, 192, 156, 67, 148, 18, 99, 33, 169, 238, 71, 131, 186, 192, 140, 2, 32, 99, 219, 236,
-//                23, 154, 161, 240, 28, 220, 207, 75, 63, 206, 164, 46, 49, 253, 222, 30, 182, 153, 139, 90, 36, 237, 56,
-//                50, 27, 237, 102, 29, 106, 129, 20, 91, 129, 44, 157, 87, 115, 30, 39, 162, 218, 139, 24, 48, 25, 95,
-//                136, 239, 50, 163, 182, 131, 20, 181, 247, 98, 121, 138, 83, 213, 67, 160, 20, 202, 248, 178, 151, 207,
-//                248, 242, 249, 55, 232
-//        };
+
+        byte [] expected = {
+                18, 0, 0, 36, 0, 0, 0, 1, 32, 27, 0, 0, 0, 0, 97, 64, 0, 0, 0, 0, 0, 3, -24, 104, 64, 0, 0, 0, 0, 0, 0,
+                10, 115, 0, 116, 71, 48, 69, 2, 33, 0, -11, 52, -67, 33, -66, -56, 90, 121, -8, -29, 64, -57, -16, -51,
+                -35, 62, 60, 86, -64, -100, 67, -108, 18, 99, 33, -87, -18, 71, -125, -70, -64, -116, 2, 32, 99, -37,
+                -20, 23, -102, -95, -16, 28, -36, -49, 75, 63, -50, -92, 46, 49, -3, -34, 30, -74, -103, -117, 90, 36,
+                -19, 56, 50, 27, -19, 102, 29, 106, -127, 20, 91, -127, 44, -99, 87, 115, 30, 39, -94, -38, -117, 24,
+                48, 25, 95, -120, -17, 50, -93, -74, -125, 20, -75, -9, 98, 121, -118, 83, -43, 67, -96, 20, -54, -8,
+                -78, -105, -49, -8, -14, -7, 55, -24
+        };
 
         byte [] signedTransaction = Signer.signTransaction(transaction, wallet);
 
-        System.out.println("Hola " + Arrays.toString(signedTransaction));
-//        assertEquals(signedTransaction, expected);
+        assertArrayEquals(signedTransaction, expected);
     }
 }
