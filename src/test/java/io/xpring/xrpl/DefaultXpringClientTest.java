79aa115be5e208bddec57ba52b6491d5a4566798
Keefer Taylor <keefer.t@gmail.com>
2020-02-05 13:22:45 -0500

    Update DefaultXpringClientTest.java

--- src/test/java/io/xpring/xrpl/DefaultXpringClientTest.java
+++ src/test/java/io/xpring/xrpl/DefaultXpringClientTest.java
@@ -399,7 +399,7 @@ public class DefaultXpringClientTest {
                             responseObserver.onCompleted();
                         }
                     }
-|                }
+                }
                 ));
     }
 
