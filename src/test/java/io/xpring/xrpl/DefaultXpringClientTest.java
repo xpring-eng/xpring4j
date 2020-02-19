f368ba9341bb0d52de3db3c5679c76edef8ca493
Keefer Taylor <keefer.t@gmail.com>
2020-02-19 17:09:20 -0500

    Update DefaultXpringClientTest.java

--- src/test/java/io/xpring/xrpl/DefaultXpringClientTest.java
+++ src/test/java/io/xpring/xrpl/DefaultXpringClientTest.java
@@ -13,6 +13,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.AdditionalAnswers.delegatesTo;
 import io.grpc.ManagedChannel;
 import io.grpc.inprocess.InProcessChannelBuilder;
+import io.xpring.xrpl.XpringKitException;	
 import rpc.v1.Amount.XRPDropsAmount;
 import rpc.v1.AccountInfo;
 import rpc.v1.FeeOuterClass.Fee;
