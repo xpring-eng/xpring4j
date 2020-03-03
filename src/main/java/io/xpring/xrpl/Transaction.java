4cb1ab64831c8380604c5da86fb5032a6113241e
Keefer Taylor <keefer.t@gmail.com>
2020-03-03 14:31:30 +0000

    add model

---
+++ src/main/java/io/xpring/xrpl/Transaction.java
@@ -0,0 +1,11 @@
+package io.xpring.xrpl;
+
+import org.immutables.value.Value;
+
+/**
+ * Represents a transaction on the XRP Ledger.
+ */
+@Value.Immutable
+public interface Transaction {
+    // TODO(keefertaylor): Implement fields here.
+}
