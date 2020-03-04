dd4691e9c8e9310a66559753daf4fb87f3b6943c
Keefer Taylor <keefer.t@gmail.com>
2020-03-04 14:21:13 +0000

    enum

---
+++ src/main/java/io/xpring/xrpl/TransactionType.java
@@ -0,0 +1,12 @@
+package io.xpring.xrpl;
+
+/**
+ * Types of transactions on the XRP Ledger.
+ *
+ * This is a partial list. Please file an issue if you have a use case that requires additional types.
+ *
+ * See: https://xrpl.org/transaction-formats.html
+ */
+public enum TransactionType {
+    PAYMENT
+}
