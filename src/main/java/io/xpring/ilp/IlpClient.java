c5eca5d5e8c41e75ccfc626782745ae680084498
nkramer44 <noah.ph.kramer@gmail.com>
2020-03-11 16:21:58 -0400

    See you never, admin client
    Signed-off-by: nkramer44 <noah.ph.kramer@gmail.com>

--- src/main/java/io/xpring/ilp/IlpClient.java
+++ src/main/java/io/xpring/ilp/IlpClient.java
@@ -1,17 +1,11 @@
 package io.xpring.ilp;
 
-import org.interledger.spsp.server.grpc.CreateAccountResponse;
-import org.interledger.spsp.server.grpc.GetAccountResponse;
-
-import com.google.common.annotations.Beta;
 import io.xpring.ilp.model.AccountBalance;
-import io.xpring.ilp.model.CreateAccountRequest;
 import io.xpring.ilp.model.PaymentRequest;
 import io.xpring.ilp.model.PaymentResult;
 import io.xpring.xrpl.XpringException;
 
 import java.math.BigInteger;
-import java.util.Optional;
 
 /**
  * A client that can create accounts, get accounts, get balances, and send ILP payments on a connector.
@@ -26,52 +20,6 @@ public class IlpClient {
      */
     public IlpClient(String grpcUrl) {
         this.decoratedClient = new DefaultIlpClient(grpcUrl);
-    }
-
-    /**
-     * Create an account on the connector.  Because no settings or auth were passed in, an account
-     * with default settings, a generated account ID, and a generated SIMPLE auth token will be generated for you.
-     *
-     * This method is currently in Beta and is likely to change or be removed.
-     *
-     * @return A {@link CreateAccountResponse} containing the account settings that were created on the connector
-     * @throws XpringException If account creation failed.
-     */
-    @Beta
-    public CreateAccountResponse createAccount() throws XpringException {
-        return decoratedClient.createAccount();
-    }
-
-    /**
-     * Create an account on the connector.
-     *
-     * This method is currently in Beta and is likely to change or be removed.
-     *
-     * @param createAccountRequest: A request object with specified account details. Note that {@link CreateAccountRequest#assetScale}
-     *                           and {@link CreateAccountRequest#assetCode} MUST be specified, as guaranteed by {@link CreateAccountRequest#builder(String, Integer)}
-     * @param bearerToken: An optionally present authentication token, for example a JWT. If no token is specified, a simple bearer
-     *                   token will be generated.
-     * @return A {@link CreateAccountResponse} containing the account settings that were created on the connector
-     * @throws XpringException If the given inputs were invalid or account creation failed.
-     */
-    @Beta
-    public CreateAccountResponse createAccount(CreateAccountRequest createAccountRequest, Optional<String> bearerToken) throws XpringException {
-        return decoratedClient.createAccount(createAccountRequest, bearerToken);
-    }
-
-    /**
-     * Gets account details for the given account ID
-     *
-     * This method is currently in Beta and is likely to change or be removed.
-     *
-     * @param accountId: Unique Identifier of this account.
-     * @param bearerToken: Authentication token to access the account.
-     * @return A {@link GetAccountResponse} with account details and settings.
-     * @throws XpringException if the given inputs were invalid, the account doesn't exist, or something else went wrong
-     */
-    @Beta
-    public GetAccountResponse getAccount(String accountId, String bearerToken) throws XpringException {
-        return decoratedClient.getAccount(accountId, bearerToken);
     }
 
     /**
