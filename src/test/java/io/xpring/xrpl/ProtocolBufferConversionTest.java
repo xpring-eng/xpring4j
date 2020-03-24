7db38d74bf4ddf26ad8d0b33657a6f0c30a4de30
Amie Corso <acorso@ripple.com>
2020-03-24 08:33:40 -0700

    rename back to ProtocolBufferConversionTest

--- src/test/java/io/xpring/xrpl/ProtocolBufferConversionTest.java
+++ src/test/java/io/xpring/xrpl/ProtocolBufferConversionTest.java
@@ -1,14 +1,10 @@
 package io.xpring.xrpl;
 
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-
 import org.junit.Test;
-import java.io.IOException;
-import java.math.BigInteger;
 
 import io.xpring.xrpl.model.XRPCurrency;
 
-public class ProtobufConversionTest {
+public class ProtocolBufferConversionTest {
     @Test
     public void convertCurrencyTest() {
         XRPCurrency xrpCurrency = XRPCurrency.from(FakeXRPProtobufs.currency);
