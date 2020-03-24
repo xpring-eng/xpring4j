647edcfde1fc31bbafa2a35fccc9a43af5541677
Amie Corso <acorso@ripple.com>
2020-03-24 11:21:18 -0700

    flesh out XRPPath

--- src/main/java/io/xpring/xrpl/model/XRPPath.java
+++ src/main/java/io/xpring/xrpl/model/XRPPath.java
@@ -1,10 +1,14 @@
 package io.xpring.xrpl.model;
 
-import com.google.protobuf.ByteString;
 import org.xrpl.rpc.v1.Payment.Path;
 import org.immutables.value.Value;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
+ * A path in the XRP Ledger.
+ * @see "https://xrpl.org/paths.html"
  */
 @Value.Immutable
 public interface XRPPath {
@@ -12,9 +16,18 @@ public interface XRPPath {
         return ImmutableXRPPath.builder();
     }
 
+    /**
+     * @return List of XRPPathElements that make up this XRPPath
+     */
+    List<XRPPathElement> pathElements();
 
     static XRPPath from(Path path) {
+        List<XRPPathElement> pathElements = path.getElementsList()
+                                            .stream()
+                                            .map(pathElement -> XRPPathElement.from(pathElement))
+                                            .collect(Collectors.toList());
         return builder()
+                .pathElements(pathElements)
                 .build();
     }
 }
