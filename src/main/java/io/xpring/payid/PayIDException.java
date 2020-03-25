b8018e02d20f9fcdee4f2266d32f688186ebe6c2
Keefer Taylor <keefer.t@gmail.com>
2020-03-25 23:17:06 +0900

    Implement Pay ID Get Call with Swagger Codegen (#137)
    * swagger plugin
    
    * add spec
    
    * rough draft of hooking up generated code
    
    * update hash
    
    * make calls with headers
    
    * intergration tests
    
    * fix tests
    
    * tests
    
    * fix ssl
    
    * honor iVar
    
    * polish
    
    * use dynamic ports
    
    * fix exception name
    
    * add visiblefortesting annotation
    
    * add http port for completeness
    
    * fix comment
    
    * less verbose find/replace

---
+++ src/main/java/io/xpring/payid/PayIDException.java
@@ -0,0 +1,26 @@
+package io.xpring.payid;
+
+public class PayIDException extends Exception {
+    /** Static exception for when a classic address is passed to an X-Address API. */
+    public static PayIDException invalidPaymentPointerException =
+            new PayIDException(PayIDExceptionType.INVALID_PAYMENT_POINTER, "Invalid Payment Pointer");
+
+    /** The type of exception. */
+    private PayIDExceptionType type;
+
+    /**
+     * Create a new exception.
+     *
+     * @param message The message to to include in the exception
+     */
+    public PayIDException(PayIDExceptionType type, String message) {
+        super(message);
+    }
+
+    /**
+     * @return The exception type.
+     */
+    public PayIDExceptionType getType() {
+        return this.type;
+    }
+}
