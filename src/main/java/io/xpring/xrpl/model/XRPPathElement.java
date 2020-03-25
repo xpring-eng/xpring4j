7afc025603fbe81cc7861155c9f480d905952591
Amie Corso <acorso@ripple.com>
2020-03-25 14:30:25 -0700

    javadoc XRPPathElement

--- src/main/java/io/xpring/xrpl/model/XRPPathElement.java
+++ src/main/java/io/xpring/xrpl/model/XRPPathElement.java
@@ -15,8 +15,27 @@ public interface XRPPathElement {
         return ImmutableXRPPathElement.builder();
     }
 
+    /**
+     * @return (Optional) If present, this path step represents rippling through the specified address.
+     * MUST NOT be provided if this path element specifies the currency or issuer fields.
+     */
     String account();
+
+    /**
+     * @return (Optional) If present, this path element represents changing currencies through an order book.
+     * The currency specified indicates the new currency.
+     * MUST NOT be provided if this path element specifies the account field.
+     */
     XRPCurrency currency();
+
+    /**
+     *
+     * @return (Optional) If present, this path element represents changing currencies and this address
+     * defines the issuer of the new currency. If omitted in a path element with a non-XRP currency,
+     * a previous element of the path defines the issuer. If present when currency is omitted,
+     * indicates a path element that uses an order book between same-named currencies with different issuers.
+     * MUST be omitted if the currency is XRP. MUST NOT be provided if this element specifies the account field.
+     */
     String issuer();
 
     static XRPPathElement from(PathElement pathElement) {
