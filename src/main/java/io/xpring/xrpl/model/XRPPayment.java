4b44def2d65b6440f563f7697615ff5c1af66c1d
Amie Corso <acorso@ripple.com>
2020-03-27 08:59:51 -0700

    fill out XRPPayment class

--- src/main/java/io/xpring/xrpl/model/XRPPayment.java
+++ src/main/java/io/xpring/xrpl/model/XRPPayment.java
@@ -4,50 +4,90 @@ import org.xrpl.rpc.v1.Payment;
 import org.immutables.value.Value;
 
 import javax.annotation.Nullable;
+import java.util.List;
+import java.util.stream.Collectors;
 
+/**
+ * A payment on the XRP Ledger.
+ * @see "https://xrpl.org/payment.html"
+ */
 @Value.Immutable
 public interface XRPPayment {
     static ImmutableXRPPayment.Builder builder() {
         return ImmutableXRPPayment.builder();
     }
 
-    // note: all optional in JS
     /**
-     * @return
+     * @return The amount of currency to deliver.
      */
     XRPCurrencyAmount amount();
 
     /**
-     * @return
+     * @return The unique address of the account receiving the payment.
      */
     String destination();
 
     /**
-     * @return
+     * @return (Optional) Arbitrary tag that identifies the reason for the payment.
      */
-    Number destinationTag();
+    int destinationTag();
 
     /**
-     * @return
+     * @return (Optional) Minimum amount of destination currency this transaction should deliver.
      */
     XRPCurrencyAmount deliverMin();
 
     /**
-     * @return
+     * @return (Optional) Arbitrary 256-bit hash representing a specific reason or identifier for this payment.
      */
-    Uint8Array invoiceID();
+    byte[] invoiceID();
 
     /**
-     * @return
+     * @return (Optional) Array of payment paths to be used for this transaction.
+     * Must be omitted for XRP-to-XRP transactions.
      */
     Array<XRPPath> paths();
 
     /**
-     * @return
+     * @return (Optional) Highest amount of source currency this transaction is allowed to cost.
      */
     XRPCurrencyAmount sendMax();
 
-    static XRPPayment from(Payment currencyAmount) {
-        return builder().build();
+    static XRPPayment from(Payment payment) {
+        // amount is required
+        XRPCurrencyAmount amount = XRPCurrencyAmount.from(payment.getAmount().getValue());
+        if (amount == null) { return null; }
+
+        // destination is required
+        String destination = payment.getDestination().getValue().getAddress();
+        if (destination.isEmpty() ) { return null; }
+
+        int destinationTag = payment.getDestinationTag().getValue();
+
+        // If the deliverMin field is set, it must be able to be transformed into an XRPCurrencyAmount.
+        XRPCurrencyAmount deliverMin = XRPCurrencyAmount.from(payment.getDeliverMin().getValue());
+        if (deliverMin == null) { return null; }
+
+        byte[] invoiceID = payment.getInvoiceId().getValue().toByteArray();
+
+        List<XRPPath> paths = payment.getPathsList()
+                                     .stream()
+                                     .map(path -> XRPPath.from(path))
+                                     .collect(Collectors.toList());
+
+        // If the sendMax field is set, it must be able to be transformed into an XRPCurrencyAmount.
+        XRPCurrencyAmount sendMax = XRPCurrencyAmount.from(payment.getSendMax().getValue());
+        if (sendMax == null) { return null; }
+
+
+        return builder()
+                .amount(amount)
+                .destination(destination)
+                .destinationTag(destinationTag)
+                .deliverMin(deliverMin)
+                .invoiceID(invoiceID)
+                .paths(paths)
+                .sendMax(sendMax)
+                .build();
     }
 }
