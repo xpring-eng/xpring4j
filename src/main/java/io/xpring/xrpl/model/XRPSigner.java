3104e4f2f068a59a2b95fafbe778cb4d4bbfc3d5
Amie Corso <acorso@ripple.com>
2020-03-29 20:19:47 -0700

    XRPSigner class

--- src/main/java/io/xpring/xrpl/model/XRPSigner.java
+++ src/main/java/io/xpring/xrpl/model/XRPSigner.java
@@ -7,8 +7,8 @@ import org.immutables.value.Value;
 import javax.annotation.Nullable;
 
 /**
- * Represents a memo on the XRPLedger.
- * @see "https://xrpl.org/transaction-common-fields.html#memos-field"
+ * Represents a signer of a transaction on the XRP Ledger.
+ * @see "https://xrpl.org/transaction-common-fields.html#signers-field"
  */
 @Value.Immutable
 public interface XRPSigner {
@@ -16,11 +16,42 @@ public interface XRPSigner {
         return ImmutableXRPSigner.builder();
     }
 
-    static XRPSigner from(Signer memo) {
+    /**
+     * @return The address associated with this signature, as it appears in the SignerList.
+     */
+    String account();
+
+    /**
+     * @return The public key used to create this signature.
+     */
+    @Nullable byte[] signingPublicKey();
+
+    /**
+     * @return A signature for this transaction, verifiable using the SigningPubKey.
+     */
+    @Nullable byte[] transactionSignature();
+
+    static XRPSigner from(Signer signer) {
+        String account = signer.getAccount().getValue().getAddress();
+        byte[] signingPublicKey;
+        byte[] transactionSignature;
+        if (account.isEmpty()) {
+            account = null;
+        }
+        if (signer.getSigningPublicKey().getValue() == ByteString.EMPTY) {
+            signingPublicKey = null;
+        } else {
+            signingPublicKey = signer.getSigningPublicKey().getValue().toByteArray();
+        }
+        if (signer.getTransactionSignature().getValue() == ByteString.EMPTY) {
+            transactionSignature = null;
+        } else {
+            transactionSignature = signer.getTransactionSignature().getValue().toByteArray();
+        }
         return XRPSigner.builder()
-                .data(data)
-                .format(format)
-                .type(type)
+                .account(account)
+                .signingPublicKey(signingPublicKey)
+                .transactionSignature(transactionSignature)
                 .build();
     }
 }
