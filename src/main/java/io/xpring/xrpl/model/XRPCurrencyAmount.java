90dce1437210bc1f57168d63972c15d8bcbb8a32
Amie Corso <acorso@ripple.com>
2020-03-31 07:15:48 -0700

    javadoc XRPCurrencyAmount.from

--- src/main/java/io/xpring/xrpl/model/XRPCurrencyAmount.java
+++ src/main/java/io/xpring/xrpl/model/XRPCurrencyAmount.java
@@ -1,5 +1,7 @@
 package io.xpring.xrpl.model;
 
+import io.xpring.ilp.model.AccountBalance;
+import org.interledger.spsp.server.grpc.GetBalanceResponse;
 import org.xrpl.rpc.v1.CurrencyAmount;
 import org.immutables.value.Value;
 import org.xrpl.rpc.v1.IssuedCurrencyAmount;
@@ -30,6 +32,15 @@ public interface XRPCurrencyAmount {
     @Nullable
     XRPIssuedCurrency issuedCurrency();
 
+    /**
+     * Constructs an {@link XRPCurrencyAmount} from a {@link CurrencyAmount}
+     * @see <a href="https://github.com/ripple/rippled/blob/develop/src/ripple/proto/org/xrpl/rpc/v1/amount.proto#L10">
+     *     CurrencyAmount protocol buffer</a>
+     *
+     * @param currencyAmount a {@link CurrencyAmount} (protobuf object) whose field values will be used
+     *                           to construct an {@link XRPCurrencyAmount}
+     * @return an {@link XRPCurrencyAmount} with its fields set via the analogous protobuf fields.
+     */
     static XRPCurrencyAmount from(CurrencyAmount currencyAmount) throws NumberFormatException {
         switch (currencyAmount.getAmountCase()) {
             // Mutually exclusive: either drops or issuedCurrency is set in an XRPCurrencyAmount
