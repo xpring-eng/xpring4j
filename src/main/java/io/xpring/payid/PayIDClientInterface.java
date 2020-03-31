cd4e751b73f50cf21b866f7c42c1127573d00b84
Keefer Taylor <keefer.t@gmail.com>
2020-04-01 02:44:12 +0900

    remove verbose comment

--- src/main/java/io/xpring/payid/PayIDClientInterface.java
+++ src/main/java/io/xpring/payid/PayIDClientInterface.java
@@ -7,8 +7,6 @@ public interface PayIDClientInterface {
     /**
      * Resolve the given PayID to an XRP Address.
      *
-     * Note: The returned value will always be in an X-Address format.
-     *
      * @param payID The payID to resolve for an address.
      * @return An XRP address representing the given PayID.
      */
