d8eaa93a90b00260ee270b6e3d824d2516e481f3
Amie Corso <acorso@ripple.com>
2020-03-31 09:28:29 -0700

    add to FakeXRPClient

--- src/test/java/io/xpring/xrpl/FakeXRPClient.java
+++ src/test/java/io/xpring/xrpl/FakeXRPClient.java
@@ -13,19 +13,22 @@ public class FakeXRPClient implements  XRPClientDecorator {
     public String sendValue;
     public int latestValidatedLedgerValue;
     public RawTransactionStatus rawTransactionStatusValue;
+    public boolean accountExistsValue;
 
     public FakeXRPClient(
             BigInteger getBalanceValue,
             TransactionStatus paymentStatusValue,
             String sendValue,
             int latestValidatedLedgerValue,
-            RawTransactionStatus rawTransactionStatusValue
+            RawTransactionStatus rawTransactionStatusValue,
+            boolean accountExistsValue
     ) {
         this.getBalanceValue = getBalanceValue;
         this.paymentStatusValue = paymentStatusValue;
         this.sendValue = sendValue;
         this.latestValidatedLedgerValue = latestValidatedLedgerValue;
         this.rawTransactionStatusValue = rawTransactionStatusValue;
+        this.accountExistsValue = accountExistsValue;
     }
 
     @Override
@@ -51,5 +54,10 @@ public class FakeXRPClient implements  XRPClientDecorator {
     @Override
     public RawTransactionStatus getRawTransactionStatus(String transactionHash) {
         return this.rawTransactionStatusValue;
+    }
+
+    @Override
+    public boolean accountExists(String address) {
+        return this.accountExistsValue;
     }
 }
