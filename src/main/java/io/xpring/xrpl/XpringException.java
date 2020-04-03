ba35b2766869b410c9a9fb3f7ef60def3f42b03d
Amie <acorso@ripple.com>
2020-04-03 10:08:17 -0700

    Implements paymentHistory (1/2) (#173)
    * remove unused import
    
    * XRPIssuedCurrency interface
    
    * organize imports
    
    * fake IssuedCurrencyAmount protos
    
    * conversion tests
    
    * few nits
    
    * XRPCurrencyAmount class started
    
    * document class
    
    * fake CurrencyAmount protobufs
    
    * document fakes
    
    * start tests
    
    * test should compare to empty object not null
    
    * test should compare to empty object not null
    
    * figure out nullable fields in these Immutables
    
    * remove unused import
    
    * remove unused imports
    
    * XRPPayment class skeleton
    
    * fill out XRPPayment class
    
    * remove unused imports
    
    * first fake Payment proto obj
    
    * finish fake payment proto objects
    
    * tests skeleton
    
    * first test case
    
    * make non-mandatory XRPPayment fields nullable
    
    * nullify any empty, optional fields
    
    * debug test paymentWithMandatoryFieldsSet
    
    * cleanup
    
    * XRPMemo class
    
    * fake memo objects
    
    * make XRPMemo fields nullable
    
    * debug XRPMemo
    
    * finish tests
    
    * start XRPSigner class
    
    * XRPSigner class
    
    * fakes and test
    
    * re-throw NumberFormatException
    
    * new line at end of file
    
    * space nit
    
    * update conversion test for re-thrown exception
    
    * camelCase
    
    * extra test and infra for handling an invalid IssuedCurrency during CurrencyAmount conversion
    
    * update XRPCurrency.from to re-throw NumberFormatException and tests
    
    * newline at end of file
    
    * newline
    
    * update tests that throw NumberFormatException
    
    * capitalize THENs
    
    * newline end of file
    
    * style cleanups
    
    * style cleanups
    
    * terminate GIVENWHENTHEN with .
    
    * XRPTransaction getting there
    
    * document XRPTransaction fields
    
    * nullables
    
    * first fake Transaction protobuf object
    
    * first conversion test
    
    * debug first test
    
    * second fake Transaction protobuf obj
    
    * second conversion test mandatory common fields only
    
    * fake Transaction with empty paymentFields
    
    * empty Payment test
    
    * unsupported txn type test
    
    * cleanup
    
    * get rid of extra vert space
    
    * move @Nullables
    
    * get rid of redundant re-throw
    
    * javadoc XRPCurrencyAmount.from
    
    * javadoc XRPCurrency.from
    
    * javadoc XRPIssuedCurrency.from
    
    * javadoc XRPPath
    
    * javadoc XRPPathElement
    
    * javadoc XRPPayment class
    
    * move @Nullables
    
    * javadoc XRPMemo.from
    
    * move @Nullables
    
    * javadoc XRPSigner.from
    
    * move @Nullables
    
    * javadoc XRPTransaction.from
    
    * move @Nullables
    
    * FakeXRPProtobufs cleanup, e | error --> exception
    
    * paymentHistory in XRPClientDecorator
    
    * delete vert space
    
    * type invlid --> invalid
    
    * error || e --> exception
    
    * put logger in FakeXRPProtobufs
    
    * add paymentConversionFailure to XpringException
    
    * paymentHistory in defaultXRPClient
    
    * paymentHistory in ReliableSubmissionXRPClient
    
    * paymentHistory in LegacyDefaultXRPClient
    
    * create fake GetAccountTransactionHistoryResponse
    
    * mocking infra for GetAccountTransactionHistoryResponse
    
    * XRPTestUtils class and transactionHistoryToPaymentsList method
    
    * first test in DefaultXRPClientTest
    
    * update existing tests with additional arg to getClient method
    
    * update FakeXRPClient with paymentHistory
    
    * update the FakeXRPClient in ReliableSubXRPClient test
    
    * fix javadoc format
    
    * logger.info --> logger.error
    
    * uncomment needed values in FakeXRPProtobufs
    
    * more tests in DefaultXRPClientTest
    
    * create mixedGetAccountTransactionHistoryResponse fake
    
    * finish fakes and tests
    
    * ReliableSub test
    
    * unused imports
    
    * more unused

--- src/main/java/io/xpring/xrpl/XpringException.java
+++ src/main/java/io/xpring/xrpl/XpringException.java
@@ -5,6 +5,10 @@ public class XpringException extends Exception {
     /** Static exception for when a classic address is passed to an X-Address API. */
     public static XpringException xAddressRequiredException = new XpringException("Please use the X-Address format. See: https://xrpaddress.info/.");
 
+    /** Static exception for when a payment transaction can't be converted to an XRPTransaction. */
+    public static XpringException paymentConversionFailure =
+            new XpringException("Could not convert payment transaction: (transaction). Please file a bug at https://github.com/xpring-eng/xpring4j/issues");
+
     /** Static exception for when functionality is unimplemented. */
     public static XpringException unimplemented = new XpringException("Unimplemented");
 
