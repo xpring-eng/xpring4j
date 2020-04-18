e0df38d6743fa35aff9730de9cafd748c0210537
Amie Corso <acorso@ripple.com>
2020-04-18 12:25:20 -0700

    Optionals in XRPCurrencyAmount

--- src/main/java/io/xpring/xrpl/model/XRPCurrencyAmount.java
+++ src/main/java/io/xpring/xrpl/model/XRPCurrencyAmount.java
@@ -3,8 +3,8 @@ package io.xpring.xrpl.model;
 
 import org.immutables.value.Value;
 import org.xrpl.rpc.v1.CurrencyAmount;
+import org.xrpl.rpc.v1.IssuedCurrencyAmount;
 
-import javax.annotation.Nullable;
 import java.util.Optional;
 
 /**
@@ -13,12 +13,11 @@ import java.util.Optional;
  * @see "https://xrpl.org/basic-data-types.html#specifying-currency-amounts"
  */
 @SuppressWarnings("checkstyle:AbbreviationAsWordInName")
-public class XRPCurrencyAmount {
-  @Nullable
-  private final String drops;
-
-  @Nullable
-  private final XRPIssuedCurrency issuedCurrency;
+@Value.Immutable
+public interface XRPCurrencyAmount {
+  static ImmutableXRPCurrencyAmount.Builder builder() {
+    return ImmutableXRPCurrencyAmount.builder();
+  }
 
   /**
    * (Optional) An amount of XRP, specified in drops.
@@ -26,9 +25,7 @@ public class XRPCurrencyAmount {
    *
    * @return The {@link String} representation of an amount of XRP, specified in drops.
    */
-  public Optional<String> drops() {
-    return Optional.ofNullable(drops);
-  }
+  Optional<String> drops();
 
   /**
    * (Optional) An amount of an issued currency.
@@ -36,9 +33,7 @@ public class XRPCurrencyAmount {
    *
    * @return The {@link XRPIssuedCurrency} of this {@link XRPCurrencyAmount}.
    */
-  public Optional<XRPIssuedCurrency> issuedCurrency() {
-    return Optional.ofNullable(issuedCurrency);
-  }
+  Optional<XRPIssuedCurrency> issuedCurrency();
 
   /**
    * Constructs an {@link XRPCurrencyAmount} from a {@link CurrencyAmount}.
@@ -49,61 +44,27 @@ public class XRPCurrencyAmount {
    * @see <a href="https://github.com/ripple/rippled/blob/develop/src/ripple/proto/org/xrpl/rpc/v1/amount.proto#L10">
    * CurrencyAmount protocol buffer</a>
    */
-  public static XRPCurrencyAmount from(CurrencyAmount currencyAmount) throws NumberFormatException {
+  static XRPCurrencyAmount from(CurrencyAmount currencyAmount) throws NumberFormatException {
     switch (currencyAmount.getAmountCase()) {
       // Mutually exclusive: either drops or issuedCurrency is set in an XRPCurrencyAmount
       case ISSUED_CURRENCY_AMOUNT: {
         if (currencyAmount.hasIssuedCurrencyAmount()) {
           XRPIssuedCurrency xrpIssuedCurrency = XRPIssuedCurrency.from(currencyAmount.getIssuedCurrencyAmount());
           if (xrpIssuedCurrency != null) {
-            return new XRPCurrencyAmountBuilder().issuedCurrency(xrpIssuedCurrency).build();
+            return builder().issuedCurrency(xrpIssuedCurrency).build();
           }
         }
-        // if AmountCase is ISSUED_CURRENCY_AMOUNT, we MUST be able to convert this to an XRPIssuedCurrency
+        // if AmountCase is ISSUED_CURRENCY_AMOUNT, we must be able to convert this to an XRPIssuedCurrency
         return null;
       }
       case XRP_AMOUNT: {
-        if (currencyAmount.hasXrpAmount()) {
-          long numericDrops = currencyAmount.getXrpAmount().getDrops();
-          String drops = Long.toString(numericDrops);
-          return new XRPCurrencyAmountBuilder().drops(drops).build();
-        }
-        // if AmountCase is XRP_AMOUNT, the protobuf MUST have an xrpAmount
-        return null;
+        long numericDrops = currencyAmount.getXrpAmount().getDrops();
+        String drops = Long.toString(numericDrops);
+        return builder().drops(drops).build();
       }
       // if no AmountCase is set (e.g. empty CurrencyAmount protobuf was passed to constructor)
       default:
         return null;
-    }
-  }
-
-  private XRPCurrencyAmount(XRPCurrencyAmount.XRPCurrencyAmountBuilder builder) {
-    this.drops = builder.drops;
-    this.issuedCurrency = builder.issuedCurrency;
-  }
-
-  //Builder class
-  public static class XRPCurrencyAmountBuilder {
-    //optional fields
-    private String drops;
-    private XRPIssuedCurrency issuedCurrency;
-
-    public XRPCurrencyAmountBuilder() {
-
-    }
-
-    public XRPCurrencyAmount.XRPCurrencyAmountBuilder drops(String drops) {
-      this.drops = drops;
-      return this;
-    }
-
-    public XRPCurrencyAmount.XRPCurrencyAmountBuilder issuedCurrency(XRPIssuedCurrency issuedCurrency) {
-      this.issuedCurrency = issuedCurrency;
-      return this;
-    }
-
-    public XRPCurrencyAmount build() {
-      return new XRPCurrencyAmount(this);
     }
   }
 }
