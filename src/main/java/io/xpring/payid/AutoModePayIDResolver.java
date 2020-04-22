f3a8c5603f9305e1db679629331b12c0ee001d73
nkramer44 <noah.ph.kramer@gmail.com>
2020-04-22 10:11:54 -0400

    fixed some todos
    Signed-off-by: nkramer44 <noah.ph.kramer@gmail.com>

--- src/main/java/io/xpring/payid/AutoModePayIDResolver.java
+++ src/main/java/io/xpring/payid/AutoModePayIDResolver.java
@@ -36,6 +36,9 @@ public class AutoModePayIDResolver implements PayIDResolver {
   protected static final String DISCOVERY_URL = "http://payid.org/rel/discovery/1.0";
   protected static final String PAY_ID_URL = "http://payid.org/rel/payid/1.0";
   protected static final String WEBFINGER_URL = ".well-known/webfinger";
+  protected static final int DISCOVERY_RETRIES = 5;
+  public static final String TEMPLATE_ACCT_PART = "{acctpart}";
+
   private OkHttpClient okHttpClient;
   private ObjectMapper objectMapper;
 
@@ -83,37 +86,39 @@ public class AutoModePayIDResolver implements PayIDResolver {
   public HttpUrl resolvePayIDUrl(PayID payID) throws PayIDDiscoveryException {
     WebFingerLink webFingerLink = this.getWebFingerPayIDLink(payID);
 
-    // TODO: Limit 5 retries.
+    int retries = 1;
     // Recurse through webfinger href responses until either the webfinger redirect doesn't exist or until
     // we get a non webfinger href URL, in which case we can infer that the href is a PayID server URL.
-    while (webFingerLink.rel().equals(DISCOVERY_URL)) {
+    while (webFingerLink.rel().equals(DISCOVERY_URL) && retries <= DISCOVERY_RETRIES) {
       String discoveryUrl = webFingerLink.href()
         .orElseThrow(() -> new PayIDDiscoveryException(PayIDDiscoveryExceptionType.UNKNOWN,
           "PayID Discovery delegation was improperly configured. Discovery URL missing href."));
       webFingerLink = this.getWebFingerPayIDLink(HttpUrl.parse(discoveryUrl));
+      retries++;
     }
 
     // On the last webfinger call, the webfinger href was invalid or doesnt exist, in which case we should fall back
     // to manual mode resolution.
+    WebFingerLink finalWebFingerLink = webFingerLink;
     String payIDUriTemplate = webFingerLink.template()
-      .orElse(webFingerLink.href()
+      .orElseGet(() -> finalWebFingerLink.href()
         .orElseThrow(() -> new PayIDDiscoveryException(PayIDDiscoveryExceptionType.UNKNOWN, "no href or template found."))
       );
     return expandUrlTemplate(payIDUriTemplate, payID);
   }
 
   /**
-   * Expands a {@link WebFingerLink#template()} if no href exists, otherwise returns the href.
+   * Expands a urlTemplate with a simple format of scheme://host/{acctpart} by replacing {acctpart} with the PayID
+   * account. The PayID Discovery RFC is very specific about the format of templates, therefore this simple
+   * replacement will always suffice for templates returned by a WebFinger query.
    *
-   * @param webFingerLink A {@link WebFingerLink} with either an href or template.
+   * @param urlTemplate A {@link String} containing a URL template.
    * @param payID A {@link PayID} which should be used to expand the template.
-   * @return The href if it exists, or the expanded template.
+   * @return The expanded template.
    */
-  // TODO: replace UriTemplate with simple simple template parser.
   protected HttpUrl expandUrlTemplate(String urlTemplate, PayID payID) {
-    UriTemplate uriTemplate = new UriTemplate(urlTemplate);
-    URI expandedTemplate = uriTemplate.expand(payID.account());
-    return HttpUrl.parse(expandedTemplate.toString());
+    String expandedTemplate = urlTemplate.replace(TEMPLATE_ACCT_PART, payID.account());
+    return HttpUrl.parse(expandedTemplate);
   }
 
   /**
@@ -122,7 +127,6 @@ public class AutoModePayIDResolver implements PayIDResolver {
    * @param payID The {@link PayID} whose host should be queried for a WebFinger Link
    * @return A present {@link WebFingerLink} containing a resolved URL or {@link Optional#empty()} if the WebFinger
    *          server was unreachable or did not provide an appropriate link.
-   * @throws JsonProcessingException if the WebFinger response could not be parsed.
    */
   protected WebFingerLink getWebFingerPayIDLink(PayID payID) {
     HttpUrl webfingerUrl = new HttpUrl.Builder()
@@ -141,18 +145,22 @@ public class AutoModePayIDResolver implements PayIDResolver {
    * @param webfingerUrl The {@link HttpUrl} of a WebFinger server endpoint.
    * @return A present {@link WebFingerLink} containing a resolved URL or {@link Optional#empty()} if the WebFinger
    *          server was unreachable or did not provide an appropriate link.
-   * @throws JsonProcessingException if the WebFinger response could not be parsed.
    */
   protected WebFingerLink getWebFingerPayIDLink(HttpUrl webfingerUrl) {
     try {
       String jrdString = this.executeForJrdString(webfingerUrl);
       WebFingerJrd typedJrd = objectMapper.readValue(jrdString, WebFingerJrd.class);
 
-      // TODO: PAY_ID_URL should take precedence over DISCOVERY_URL
       return typedJrd.links().stream()
-        .filter(link -> link.rel().equals(DISCOVERY_URL) || link.rel().equals(PAY_ID_URL))
+        .filter(link -> link.rel().equals(PAY_ID_URL))
         .findFirst()
-        .orElseThrow(() -> new PayIDDiscoveryException(PayIDDiscoveryExceptionType.UNKNOWN, "No acceptable link rel found."));
+        .orElseGet(() -> typedJrd.links().stream()
+          .filter(link -> link.rel().equals(DISCOVERY_URL))
+          .findFirst()
+          .orElseThrow(() -> new PayIDDiscoveryException(
+            PayIDDiscoveryExceptionType.UNKNOWN,
+            "No acceptable link rel found.")
+          ));
     } catch (JsonProcessingException e) {
       logger.warn("Unable to deserialize WebFinger JRD! message: {}", e.getMessage());
       throw new PayIDDiscoveryException(PayIDDiscoveryExceptionType.UNKNOWN, "Unable to deserialize WebFinger JRD!");
