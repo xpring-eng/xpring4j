aabeaecf3774a721e514900ebb9233e7ffdd08c3
Keefer Taylor <keefer.t@gmail.com>
2020-06-25 15:54:42 +0900

    fix some build failures

--- src/test/java/io/xpring/payid/PayIdIntegrationTest.java
+++ src/test/java/io/xpring/payid/PayIdIntegrationTest.java
@@ -2,26 +2,16 @@ package io.xpring.payid;
 
 import static org.junit.Assert.assertEquals;
 
-<<<<<<< HEAD:src/test/java/io/xpring/payid/PayIdIntegrationTest.java
 import io.xpring.common.XrplNetwork;
-=======
-import io.xpring.common.XRPLNetwork;
->>>>>>> origin/master:src/test/java/io/xpring/payid/PayIDIntegrationTest.java
 import io.xpring.payid.generated.model.Address;
 import io.xpring.payid.generated.model.CryptoAddressDetails;
-import io.xpring.payid.idiomatic.PayIdException;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import java.util.List;
 
-<<<<<<< HEAD:src/test/java/io/xpring/payid/PayIdIntegrationTest.java
 public class PayIdIntegrationTest {
-=======
-@SuppressWarnings("checkstyle:AbbreviationAsWordInName")
-public class PayIDIntegrationTest {
->>>>>>> origin/master:src/test/java/io/xpring/payid/PayIDIntegrationTest.java
   @Rule
   public ExpectedException expectedException = ExpectedException.none();
 
@@ -70,37 +60,22 @@ public class PayIDIntegrationTest {
   @SuppressWarnings("checkstyle:AbbreviationAsWordInName")
   public void testBTCAddressForKnownAddressTestnet() throws PayIdException {
     // GIVEN a PayID that will resolve on BTC testnet.
-<<<<<<< HEAD:src/test/java/io/xpring/payid/PayIdIntegrationTest.java
     PayIdClient payIdClient = new PayIdClient();
-=======
-    PayIDClient payIDClient = new PayIDClient();
->>>>>>> origin/master:src/test/java/io/xpring/payid/PayIDIntegrationTest.java
     String payID = "alice$dev.payid.xpring.money";
     String network = "btc-testnet";
 
     // WHEN it is resolved to a BTC address
-<<<<<<< HEAD:src/test/java/io/xpring/payid/PayIdIntegrationTest.java
     CryptoAddressDetails btcAddressDetails = payIdClient.cryptoAddressForPayId(payID, network);
-=======
-    CryptoAddressDetails btcAddressDetails = payIDClient.cryptoAddressForPayId(payID, network);
->>>>>>> origin/master:src/test/java/io/xpring/payid/PayIDIntegrationTest.java
 
     // THEN the address is the expected value.
     assertEquals(btcAddressDetails.getAddress(), "2NF9H32iwQcVcoAiiBmAtjpGmQfsmU5L6SR");
   }
 
   @Test
-<<<<<<< HEAD:src/test/java/io/xpring/payid/PayIdIntegrationTest.java
-  public void testAllAddressesForPayId() throws PayIdException, PayIdException {
+  public void testAllAddressesForPayId() throws PayIdException {
     // GIVEN a PayID with multiple addresses.
     String payId = "alice$dev.payid.xpring.money";
     PayIdClient payIdClient = new PayIdClient();
-=======
-  public void testAllAddressesForPayId() throws PayIdException, PayIDException {
-    // GIVEN a PayID with multiple addresses.
-    String payId = "alice$dev.payid.xpring.money";
-    PayIDClient payIdClient = new PayIDClient();
->>>>>>> origin/master:src/test/java/io/xpring/payid/PayIDIntegrationTest.java
 
     // WHEN the PayID is resolved to a set of addresses.
     List<Address> addresses = payIdClient.allAddressesForPayId(payId);
