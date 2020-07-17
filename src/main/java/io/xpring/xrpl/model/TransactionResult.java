45b525c17ccd4d5880aab0b202cf4fae2a395fe1
Amie Corso <acorso@ripple.com>
2020-07-17 16:17:29 -0700

    update TransactionResult class to include validated field

--- src/main/java/io/xpring/xrpl/model/TransactionResult.java
+++ src/main/java/io/xpring/xrpl/model/TransactionResult.java
@@ -3,21 +3,23 @@ package io.xpring.xrpl.model;
 import io.xpring.xrpl.TransactionStatus;
 
 /**
- * Represents the outcome of submitting an XRPL transaction. The hash is the identifying hash of the transaction.
- * The status is the final result of the transaction.
- * <p>
- * Note: This class is intended to be used with reliable submission logic, and the status represents the final
- * outcome of the transaction after inclusion in a validated ledger, or after the expiration of the transaction's
- * lastLedgerSequence.  For example, a successful status indicates that the transaction was applied in a validated
- * ledger with a successful result code, as opposed to success only on an open ledger in a local rippled environment.
- * </p>
+ * Represents the outcome of submitting an XRPL transaction.
  */
 public class TransactionResult {
   String hash;
   TransactionStatus status;
+  Boolean validated;
 
-  public TransactionResult(String hash, TransactionStatus status) {
+  /**
+   *
+   * @param hash The identifying hash of the transaction.
+   * @param status The {@link TransactionStatus} indicating the outcome of this transaction.
+   * @param validated Whether this transaction is included in a validated ledger.
+   *                  The transactions status is only final if this field is true.
+   */
+  public TransactionResult(String hash, TransactionStatus status, Boolean validated) {
     this.hash = hash;
     this.status = status;
+    this.validated = validated;
   }
 }
