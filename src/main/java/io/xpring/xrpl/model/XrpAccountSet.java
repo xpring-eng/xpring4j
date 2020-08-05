bd4c93369c1dd43280703343b9cbafe1777c6903
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-05 11:30:18 -0700

    Adds more implementation

--- src/main/java/io/xpring/xrpl/model/XrpAccountSet.java
+++ src/main/java/io/xpring/xrpl/model/XrpAccountSet.java
@@ -1,5 +1,25 @@
 package io.xpring.xrpl.model;
 
-public interface XrpAccountSet {
-}
+import io.xpring.xrpl.model.ImmutableXrpAccountSet;
+import org.immutables.value.Value;
 
+@Value.Immutable
+public interface XrpAccountSet {
+    static ImmutableXrpAccountSet.Builder build() { return ImmutableXrpAccountSet.builder(); }
+
+    int clearFlag();
+
+    java.lang.String domain();
+
+    // Convert this in ::from from the pb itself
+    byte[] emailHash();
+
+    // Convert this in ::from from the pb itself
+    byte[] messageKey();
+
+    Integer setFlag();
+
+    Integer transferRate();
+
+    Integer tickSize();
+}
