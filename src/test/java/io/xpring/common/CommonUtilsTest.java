fcb71140909025a22de3219142909043abbe54cc
Amie Corso <acorso@ripple.com>
2020-08-05 11:24:43 -0700

    common utils test

---
+++ src/test/java/io/xpring/common/CommonUtilsTest.java
@@ -0,0 +1,59 @@
+package io.xpring.common;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link CommonUtils}.
+ */
+public class CommonUtilsTest {
+
+  @Test
+  public void testStringToHexConvertsStringCorrectly() {
+    // GIVEN the best line delivered on film
+    // WHEN that line is converted to hex
+    // THEN the hex value is correct
+    assertEquals(CommonUtils.stringToHex("oh, hi mark"), "6f682c206869206d61726b");
+  }
+
+  @Test
+  public void testStringToHexEmptyStringHandledCorrectly() {
+    // GIVEN an empty string
+    // WHEN that string is converted to hex
+    // THEN the hex value is also empty
+    assertEquals(CommonUtils.stringToHex(""), "");
+  }
+
+  @Test
+  public void testStringToByteArrayHandlesBlankValuesCorrectly() {
+    // GIVEN an empty string
+    // WHEN that line is converted to a byte[]
+    // THEN the value is undefined
+    assertEquals(CommonUtils.stringToByteArray("", false), new byte[0]);
+  }
+
+  @Test
+  public void testStringToByteArrayHandlesNonHexValuesCorrectly() {
+    // GIVEN the best line delivered on film
+    // WHEN that line is converted to a byte[]
+    // THEN the value is correct
+    byte expectedBytes[] = {111, 104, 44, 32, 104, 105, 32, 109, 97, 114, 107};
+    assertEquals(
+            CommonUtils.stringToByteArray("oh, hi mark", false),
+            expectedBytes
+            );
+  }
+
+  @Test
+  public void testStringToByteArrayHandlesHexValuesCorrectly() {
+    // GIVEN the hex version of the best line delivered on film
+    // WHEN that line is converted to a byte[]
+    // THEN the value is correct
+    byte expectedBytes[] = {111, 104, 44, 32, 104, 105, 32, 109, 97, 114, 107};
+    assertEquals(
+            CommonUtils.stringToByteArray("6f682c206869206d61726b", true),
+            expectedBytes
+            );
+  }
+}
