74756ac97a4997a2fd12b46fd050c3cff7035c74
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-05 17:17:43 -0700

    Finishes from method

--- src/main/java/io/xpring/xrpl/model/XrpAccountSet.java
+++ src/main/java/io/xpring/xrpl/model/XrpAccountSet.java
@@ -3,6 +3,8 @@ package io.xpring.xrpl.model;
 import io.xpring.xrpl.model.ImmutableXrpAccountSet;
 import org.immutables.value.Value;
 
+import org.xrpl.rpc.v1.AccountSet;
+
 import java.util.Optional;
 
 /**
@@ -60,6 +62,15 @@ public interface XrpAccountSet {
   Optional<Integer> setFlag();
 
   /**
+   * (Optional) Tick size to use for offers involving a currency issued by this address.
+   * The exchange rates of those offers is rounded to this many significant digits.
+   * Valid values are 3 to 15 inclusive, or 0 to disable. (Requires the TickSize amendment.)
+   *
+   * @return An integer representing the tick size to use for offers involving a currency issued by this address.
+   */
+  Optional<Integer> tickSize();
+
+  /**
    * (Optional) The fee to charge when users transfer this account's issued currencies,
    * represented as billionths of a unit.
    * Cannot be more than 2000000000 or less than 1000000000, except for the special case
@@ -70,12 +81,45 @@ public interface XrpAccountSet {
    */
   Optional<Integer> transferRate();
 
-  /**
-   * (Optional) Tick size to use for offers involving a currency issued by this address.
-   * The exchange rates of those offers is rounded to this many significant digits.
-   * Valid values are 3 to 15 inclusive, or 0 to disable. (Requires the TickSize amendment.)
-   *
-   * @return An integer representing the tick size to use for offers involving a currency issued by this address.
-   */
-  Optional<Integer> tickSize();
+  static XrpAccountSet from(AccountSet accountSet) {
+
+    Optional<Integer> clearFlag = Optional.empty();
+    if (accountSet.hasClearFlag()) {
+      clearFlag = Optional.of(accountSet.getClearFlag().getValue());
+    }
+
+    Optional<String> domain = Optional.empty();
+    if (accountSet.hasDomain()) {
+      domain = Optional.of(accountSet.getDomain().getValue());
+    }
+
+    final byte[] emailHash = accountSet.getEmailHash().getValue().toByteArray();
+
+    final byte[] messageKey = accountSet.getMessageKey().getValue().toByteArray();
+
+    Optional<Integer> setFlag = Optional.empty();
+    if (accountSet.hasSetFlag()) {
+      setFlag = Optional.of(accountSet.getSetFlag().getValue());
+    }
+
+    Optional<Integer> tickSize = Optional.empty();
+    if (accountSet.hasTickSize()) {
+      tickSize = Optional.of(accountSet.getTickSize().getValue());
+    }
+
+    Optional<Integer> transferRate = Optional.empty();
+    if (accountSet.hasTransferRate()) {
+      transferRate = Optional.of(accountSet.getTransferRate().getValue());
+    }
+
+    return XrpAccountSet.builder()
+      .clearFlag(clearFlag)
+      .domain(domain)
+      .emailHash(emailHash)
+      .messageKey(messageKey)
+      .setFlag(setFlag)
+      .tickSize(tickSize)
+      .transferRate(transferRate)
+      .build();
+  }
 }
