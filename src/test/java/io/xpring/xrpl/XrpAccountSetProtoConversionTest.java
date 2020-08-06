53ef01b1fdece8356159746be24d42afe0497010
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-06 14:05:55 -0700

    Initialize XrpAccountSet vars

--- src/test/java/io/xpring/xrpl/XrpAccountSetProtoConversionTest.java
+++ src/test/java/io/xpring/xrpl/XrpAccountSetProtoConversionTest.java
@@ -1,15 +1,16 @@
 package io.xpring.xrpl;
 
+import io.xpring.xrpl.model.XrpAccountSet;
 import org.junit.Test;
 
 public class XrpAccountSetProtoConversionTest {
   @Test
   public void allFieldsSetTest() {
-
+    final XrpAccountSet accountSet = XrpAccountSet.from(FakeXrpProtobufs.allFieldsAccountSet);
   }
 
   @Test
   public void oneFieldSetTest() {
-
+    final XrpAccountSet accountSet = XrpAccountSet.from(FakeXrpProtobufs.oneFieldAccountSet);
   }
 }
