12048b0041bdf64376e9e78d5db876e74f1a3231
Amie Corso <acorso@ripple.com>
2020-08-10 16:18:04 -0700

    fix deprecations

--- src/main/java/io/xpring/common/CommonUtils.java
+++ src/main/java/io/xpring/common/CommonUtils.java
@@ -34,6 +34,22 @@ public class CommonUtils {
   }
 
   /**
+   * Convert a hex string to bytes.
+   *
+   * @param hex The hex to convert to bytes.
+   * @return Bytes from hex.
+   */
+  public static byte[] hexStringToByteArray(String hex) {
+    int len = hex.length();
+    byte[] data = new byte[len / 2];
+    for (int i = 0; i < len; i += 2) {
+      data[i / 2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4)
+              + Character.digit(hex.charAt(i + 1), 16));
+    }
+    return data;
+  }
+
+  /**
    * Convert bytes to a hex string.
    *
    * @param bytes The bytes to convert.
