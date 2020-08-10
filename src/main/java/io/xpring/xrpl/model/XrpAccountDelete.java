467caf2ea1aff6cfa871bf148aea7fdd922fbd2b
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-10 15:05:52 -0700

    Formatting

--- src/main/java/io/xpring/xrpl/model/XrpAccountDelete.java
+++ src/main/java/io/xpring/xrpl/model/XrpAccountDelete.java
@@ -44,7 +44,9 @@ public interface XrpAccountDelete {
    *
    * @param accountDelete An {@link AccountDelete} (protobuf object) whose field values will be used
    *                      to construct an XrpAccountDelete
+   *
    * @return An {@link XrpAccountDelete} with its fields set via the analogous protobuf fields.
+   *
    * @see <a href="https://github.com/ripple/rippled/blob/3d86b49dae8173344b39deb75e53170a9b6c5284/src/ripple/proto/org/xrpl/rpc/v1/transaction.proto#L118">
    * AccountDelete protocol buffer</a>
    */
