2eeb91f7c16b8b5d4d36075d1170ce678f9b93f0
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-11 16:51:31 -0700

    Adds docs

--- src/main/java/io/xpring/xrpl/model/XrpCheckCreate.java
+++ src/main/java/io/xpring/xrpl/model/XrpCheckCreate.java
@@ -30,9 +30,31 @@ public interface XrpCheckCreate {
    */
   String destinationXAddress();
 
+  /**
+   * (Optional) Time after which the Check is no longer valid, in seconds since the Ripple Epoch.
+   *
+   * @return An {@link Integer} representing the time after which the Check is no longer valid, in seconds
+   *         since the Ripple Epoch.
+   */
   Optional<Integer> expiration();
 
+  /**
+   * (Optional) Arbitrary 256-bit hash representing a specific reason or identifier for this Check.
+   *
+   * @return A {@link String} containing an arbitrary 256-bit hash representing a specific reason or identifier for
+   *         this Check.
+   */
   Optional<String> invoiceID();
 
+  /**
+   * Maximum amount of source currency the Check is allowed to debit the sender, including transfer fees on non-XRP
+   * currencies.
+   * <p>
+   * The Check can only credit the destination with the same currency (from the same issuer, for non-XRP currencies).
+   * For non-XRP amounts, the nested field names MUST be lower-case.
+   * </p>
+   * @return A {@link XrpCurrencyAmount} representing the maximum amount of source currency the Check is allowed
+   *         to debit the sender, including transfer fees on non-XRP currencies.
+   */
   Optional<XrpCurrencyAmount> sendMax();
 }
