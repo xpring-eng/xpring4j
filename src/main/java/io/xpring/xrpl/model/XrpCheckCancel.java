594050b5962a3d6b64a77c574ea25928142c387f
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-11 11:02:04 -0700

    Initial commit

--- src/main/java/io/xpring/xrpl/model/XrpCheckCancel.java
+++ src/main/java/io/xpring/xrpl/model/XrpCheckCancel.java
@@ -1,7 +1,6 @@
 package io.xpring.xrpl.model;
 
 import org.immutables.value.Value;
-import org.xrpl.rpc.v1.CheckCancel;
 
 /**
  * Represents a CheckCancel transaction on the XRP Ledger.
@@ -17,32 +16,5 @@ import org.xrpl.rpc.v1.CheckCancel;
 public interface XrpCheckCancel {
   static ImmutableXrpCheckCancel.Builder builder() {
     return ImmutableXrpCheckCancel.builder();
-  }
-
-  /**
-   * The ID of the Check ledger object to cancel, as a 64-character hexadecimal string.
-   *
-   * @return A {@link String} representing the ID of the Check ledger object to cancel, as a 64-character hexadecimal
-   *         string.
-   */
-  String checkId();
-
-  /**
-   * Constructs an XrpCheckCancel from a CheckCancel protocol buffer.
-   *
-   * @param checkCancel A {@link CheckCancel} (protobuf object) whose field values will be used to construct an
-   *                    XrpCheckCancel
-   * @return An {@link XrpCheckCancel} with its fields set via the analogous protobuf fields.
-   * @see <a href="https://github.com/ripple/rippled/blob/3d86b49dae8173344b39deb75e53170a9b6c5284/src/ripple/proto/org/xrpl/rpc/v1/transaction.proto#L126">
-   * CheckCancel protocol buffer</a>
-   */
-  static XrpCheckCancel from(CheckCancel checkCancel) {
-    if (!checkCancel.hasCheckId() || checkCancel.getCheckId().getValue().isEmpty()) {
-      return null;
-    }
-
-    return builder()
-        .checkId(checkCancel.getCheckId().getValue().toString())
-        .build();
   }
 }
