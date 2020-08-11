1ca79200e8b759a541eddc8a4c8ee04fed326e46
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-11 13:55:04 -0700

    Adds dfrom

--- src/main/java/io/xpring/xrpl/model/XrpCheckCash.java
+++ src/main/java/io/xpring/xrpl/model/XrpCheckCash.java
@@ -28,7 +28,65 @@ public interface XrpCheckCash {
    */
   String checkId();
 
+  /**
+   * (Optional) Redeem the Check for exactly this amount, if possible.
+   * <p>
+   * The currency must match that of the SendMax of the corresponding CheckCreate transaction.
+   * You must provide either this field or deliverMin.
+   * </p>
+   *
+   * @return A {@link XrpCurrencyAmount} representing the amount to redeem the Check for, if possible.
+   */
   Optional<XrpCurrencyAmount> amount();
 
+  /**
+   * (Optional) Redeem the Check for at least this amount and for as much as possible.
+   * <p>
+   * The currency must match that of the SendMax of the corresponding CheckCreate transaction.
+   * You must provide either this field or amount.
+   * </p>
+   *
+   * @return A {@link XrpCurrencyAmount} representing the minimum amount to redeem the Check for, and for as much as
+   *         possible.
+   */
   Optional<XrpCurrencyAmount> deliverMin();
+
+  /**
+   * Constructs an XrpCheckCash from a CheckCash protocol buffer.
+   *
+   * @param checkCash A {@link CheckCash} (protobuf object) whose field values will be used to construct an XrpCheckCash
+   * @return an XrpCheckCash with its fields set via the analogous protobuf fields.
+   * @see <a href="https://github.com/ripple/rippled/blob/3d86b49dae8173344b39deb75e53170a9b6c5284/src/ripple/proto/org/xrpl/rpc/v1/transaction.proto#L132>
+   * CheckCash protocol buffer</a>
+   */
+  static XrpCheckCash from(CheckCash checkCash) {
+    // If the amount field is set, it must be able to be transformed into an XrpCurrencyAmount.
+    Optional<XrpCurrencyAmount> amount = Optional.empty();
+    if (checkCash.hasDeliverMin()) {
+      amount = Optional.ofNullable(XrpCurrencyAmount.from(checkCash.getAmount().getValue()));
+      if (!amount.isPresent()) {
+        return null;
+      }
+    }
+
+    if (!checkCash.hasCheckId() || checkCash.getCheckId().getValue().isEmpty()) {
+      return null;
+    }
+    final String checkId = checkCash.getCheckId().getValue().toString();
+
+    // If the deliverMin field is set, it must be able to be transformed into an XrpCurrencyAmount.
+    Optional<XrpCurrencyAmount> deliverMin = Optional.empty();
+    if (checkCash.hasDeliverMin()) {
+      deliverMin = Optional.ofNullable(XrpCurrencyAmount.from(checkCash.getDeliverMin().getValue()));
+      if (!deliverMin.isPresent()) {
+        return null;
+      }
+    }
+
+    return builder()
+        .amount(amount)
+        .checkId(checkId)
+        .deliverMin(deliverMin)
+        .build();
+  }
 }
