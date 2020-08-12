6c7b7d028fc273fb166a5329e7e55c44165c6566
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-12 12:33:08 -0700

    Adds a test

--- src/test/java/io/xpring/xrpl/XrpCheckCreateProtoConversionTest.java
+++ src/test/java/io/xpring/xrpl/XrpCheckCreateProtoConversionTest.java
@@ -3,11 +3,8 @@ package io.xpring.xrpl;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 import io.xpring.common.XrplNetwork;
-import io.xpring.xrpl.model.XrpCheckCash;
 import io.xpring.xrpl.model.XrpCheckCreate;
-import io.xpring.xrpl.model.XrpCurrencyAmount;
 import org.junit.Test;
-import org.xrpl.rpc.v1.CheckCash;
 import org.xrpl.rpc.v1.CheckCreate;
 
 public class XrpCheckCreateProtoConversionTest {
@@ -20,50 +17,19 @@ public class XrpCheckCreateProtoConversionTest {
     final XrpCheckCreate checkCreate = XrpCheckCreate.from(checkCreateProto, XrplNetwork.TEST);
 
     ClassicAddress classicAddress = ImmutableClassicAddress.builder()
-        .address(checkCreateProto.getDestination().getValue().getAddress())
-        .tag(checkCreateProto.getDestinationTag().getValue())
-        .isTest(true)
-        .build();
+      .address(checkCreateProto.getDestination().getValue().getAddress())
+      .tag(checkCreateProto.getDestinationTag().getValue())
+      .isTest(true)
+      .build();
     final String destinationXAddress = Utils.encodeXAddress(classicAddress);
 
     // THEN the CheckCreate converted as expected.
     assertThat(checkCreate.destinationXAddress()).isEqualTo(destinationXAddress);
     assertThat(checkCreate.expiration().get()).isEqualTo(checkCreateProto.getExpiration().getValue());
-    assertThat(checkCreate.invoiceId().get()).isEqualTo(checkCreateProto.getInvoiceId().getValue().toString());
-    assertThat(checkCreate.sendMax().get()).isEqualTo(XrpCurrencyAmount.from(checkCreateProto.getSendMax().getValue()));
+    assertThat(checkCreate.invoiceID().get()).isEqualTo(checkCreateProto.getInvoiceId().getValue());
+    assertThat(checkCreate.sendMax().get()).isEqualTo(checkCreateProto.getSendMax().getValue());
   }
 
   @Test
-  public void checkCreateMandatoryFieldsSetTest() {
-    // GIVEN a CheckCreate protocol buffer with only mandatory fields set.
-    final CheckCreate checkCreateProto = FakeXrpTransactionProtobufs.checkCreateProtoWithMandatoryFields;
-
-    // WHEN the protocol buffer is converted to a native Java object.
-    final XrpCheckCreate checkCreate = XrpCheckCreate.from(checkCreateProto, XrplNetwork.TEST);
-
-    ClassicAddress classicAddress = ImmutableClassicAddress.builder()
-        .address(checkCreateProto.getDestination().getValue().getAddress())
-        .isTest(true)
-        .build();
-    final String destinationXAddress = Utils.encodeXAddress(classicAddress);
-
-    // THEN the CheckCreate converted as expected.
-    assertThat(checkCreate.destinationXAddress()).isEqualTo(destinationXAddress);
-    assertThat(checkCreate.expiration()).isEmpty();
-    assertThat(checkCreate.invoiceId()).isEmpty();
-    assertThat(checkCreate.sendMax().get()).isEqualTo(XrpCurrencyAmount.from(checkCreateProto.getSendMax().getValue()));
-  }
-
-  @Test
-  public void invalidCheckCreateMissingDestinationTest() {
-    // GIVEN an invalid CheckCreate protocol buffer missing the destination field.
-    final CheckCreate checkCreateProto = FakeXrpTransactionProtobufs.invalidCheckCreateProto;
-
-    // WHEN the protocol buffer is converted to a native Java object.
-    final XrpCheckCreate checkCreate = XrpCheckCreate.from(checkCreateProto, XrplNetwork.TEST);
-
-    // THEN the result is null.
-    assertThat(checkCreate).isNull();
-  }
-
+  public void checkCreateMandatoryFieldsSetTest() {}
 }
