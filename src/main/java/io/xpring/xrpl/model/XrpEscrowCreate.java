bdfa1dd0e3f6da2fa06df8696280e5a660fa88b2
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-17 12:58:42 -0700

    Adds doc for amount

--- src/main/java/io/xpring/xrpl/model/XrpEscrowCreate.java
+++ src/main/java/io/xpring/xrpl/model/XrpEscrowCreate.java
@@ -4,12 +4,29 @@ import org.immutables.value.Value;
 
 import java.util.Optional;
 
+/**
+ * Represents an EscrowCreate transaction on the XRP Ledger.
+ *
+ * An EscrowCreate transaction sequesters XRP until the escrow process either finishes or is canceled.
+ *
+ * @see "https://xrpl.org/escrowcreate.html"
+ */
 @Value.Immutable
 public interface XrpEscrowCreate {
   static ImmutableXrpEscrowCreate.Builder builder() {
     return ImmutableXrpEscrowCreate.builder();
   }
 
+  /**
+   * Amount of XRP, in drops, to deduct from the sender's balance and escrow.
+   * <p>
+   * Once escrowed, the XRP can either go to the Destination address (after the FinishAfter time)
+   * or returned to the sender (after the CancelAfter time).
+   * </p>
+   * @return An {@link XrpCurrencyAmount} containing the amount of XRP, in drops, to deduct from the sender's balance and escrow.
+   *         Once escrowed, the XRP can either go to the Destination address (after the FinishAfter time)
+   *         or returned to the sender (after the CancelAfter time).
+   */
   XrpCurrencyAmount amount();
 
   Optional<Integer> cancelAfter();
@@ -17,6 +34,6 @@ public interface XrpEscrowCreate {
   Optional<String> condition();
 
   String destinationXAddress();
-  
+
   Optional<Integer> finishAfter();
 }
