ce47d71d7cb5263fad97feed12ea0bd5e998683e
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-17 10:49:15 -0700

    Adds skeleton for tests

---
+++ src/test/java/io/xpring/xrpl/XrpEscrowCancelProtoConversionTest.java
@@ -0,0 +1,13 @@
+package io.xpring.xrpl;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import org.junit.Test;
+
+public class XrpEscrowCancelProtoConversionTest {
+  @Test
+  public void escrowCancelConversionTest() {}
+
+  @Test
+  public void invalidEscrowCancelMissingOwnerTest() {}
+}
