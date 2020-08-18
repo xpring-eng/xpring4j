97de6d11fe0d5609975e37a19417bcb0a23bce4c
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-18 14:41:44 -0700

    Adds some tests

--- src/test/java/io/xpring/xrpl/UtilsTest.java
+++ src/test/java/io/xpring/xrpl/UtilsTest.java
@@ -1,8 +1,6 @@
 package io.xpring.xrpl;
 
-import static io.xpring.xrpl.Utils.dropsToXrp;
-import static io.xpring.xrpl.Utils.isTestNetwork;
-import static io.xpring.xrpl.Utils.xrpToDrops;
+import static io.xpring.xrpl.Utils.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
