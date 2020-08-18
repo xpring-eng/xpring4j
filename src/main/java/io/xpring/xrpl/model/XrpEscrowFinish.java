0b253d5f54fbda38657e7153347ea2f20576a2c3
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-18 15:20:55 -0700

    Adds accessor definitions for XrpEscrowFinish

--- src/main/java/io/xpring/xrpl/model/XrpEscrowFinish.java
+++ src/main/java/io/xpring/xrpl/model/XrpEscrowFinish.java
@@ -1,4 +1,20 @@
 package io.xpring.xrpl.model;
 
+import org.immutables.value.Value;
+
+import java.util.Optional;
+
+@Value.Immutable
 public interface XrpEscrowFinish {
+  static ImmutableXrpEscrowFinish.Builder builder() {
+    return ImmutableXrpEscrowFinish.builder();
+  }
+
+  String ownerXAddress();
+
+  Integer offerSequence();
+
+  Optional<String> condition();
+
+  Optional<String> fulfillment();
 }
