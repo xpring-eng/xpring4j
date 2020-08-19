c458eaf26461bf9c2c1b5b8ad662c1b6f4fc1bb2
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-19 14:09:51 -0700

    Adds more doc

--- src/main/java/io/xpring/xrpl/model/XrpPaymentChannelClaim.java
+++ src/main/java/io/xpring/xrpl/model/XrpPaymentChannelClaim.java
@@ -19,8 +19,25 @@ public interface XrpPaymentChannelClaim {
     return ImmutableXrpPaymentChannelClaim.builder();
   }
 
+  /**
+   * (Optional) The amount of XRP, in drops, authorized by the Signature.
+   * <p>
+   * This must match the amount in the signed message.
+   * </p>
+   *
+   */
   Optional<XrpCurrencyAmount> amount();
 
+  /**
+   * (Optional) Total amount of XRP, in drops, delivered by this channel after processing this claim.
+   * <p>
+   * Required to deliver XRP. Must be more than the total amount delivered by the channel so far,
+   * but not greater than the Amount of the signed claim. Must be provided except when closing the channel.
+   * </p>
+   *
+   * @return An {@link XrpCurrencyAmount} containing the total amount of XRP, in drops, delivered by this channel
+   *         after processing this claim.
+   */
   Optional<XrpCurrencyAmount> balance();
 
   /**
@@ -30,7 +47,27 @@ public interface XrpPaymentChannelClaim {
    */
   String channel();
 
+  /**
+   * (Optional) The public key used for the signature, as hexadecimal.
+   * <p>
+   * This must match the PublicKey stored in the ledger for the channel. Required unless the sender of the transaction
+   * is the source address of the channel and the Signature field is omitted.
+   * (The transaction includes the PubKey so that rippled can check the validity of the signature before trying to
+   * apply the transaction to the ledger.)
+   * </p>
+   *
+   * @return A {@link String} containing the public key used for the signature, as hexadecimal.
+   */
   Optional<String> publicKey();
 
+  /**
+   * (Optional) The signature of this claim, as hexadecimal.
+   * <p>
+   * The signed message contains the channel ID and the amount of the claim.
+   * Required unless the sender of the transaction is the source address of the channel.
+   * </p>
+   *
+   * @return A {@link String} containing the signature of this claim, as hexadecimal.
+   */
   Optional<String> signature();
 }
