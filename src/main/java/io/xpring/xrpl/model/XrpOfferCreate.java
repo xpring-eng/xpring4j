08a566138e6f780cb292e26dd7411d09a8079a09
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-19 12:12:38 -0700

    Adds properties

--- src/main/java/io/xpring/xrpl/model/XrpOfferCreate.java
+++ src/main/java/io/xpring/xrpl/model/XrpOfferCreate.java
@@ -1,4 +1,20 @@
 package io.xpring.xrpl.model;
 
-public class XrpOfferCreate {
+import org.immutables.value.Value;
+
+import java.util.Optional;
+
+@Value.Immutable
+public interface XrpOfferCreate {
+  static ImmutableXrpOfferCreate.Builder builder() {
+    return ImmutableXrpOfferCreate.builder();
+  }
+
+  Optional<Integer> expiration();
+
+  Optional<Integer> offerSequence();
+
+  XrpCurrencyAmount takerGets();
+
+  XrpCurrencyAmount takerPays();
 }
