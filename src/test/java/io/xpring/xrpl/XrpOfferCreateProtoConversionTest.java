81c00a0a77afb5d9553a1072f5b993de08237520
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-19 12:31:53 -0700

    Adds required fields test

--- src/test/java/io/xpring/xrpl/XrpOfferCreateProtoConversionTest.java
+++ src/test/java/io/xpring/xrpl/XrpOfferCreateProtoConversionTest.java
@@ -1,10 +1,28 @@
 package io.xpring.xrpl;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import io.xpring.xrpl.model.XrpCurrencyAmount;
+import io.xpring.xrpl.model.XrpOfferCreate;
 import org.junit.Test;
+import org.xrpl.rpc.v1.OfferCreate;
 
 public class XrpOfferCreateProtoConversionTest {
   @Test
   public void offerCreateRequiredFieldsTest() {
+    // GIVEN a valid CheckCash protocol buffer with required fields set.
+    final OfferCreate offerCreateProto = FakeXrpTransactionProtobufs.offerCreateWithRequiredFields;
 
+    // WHEN the protocol buffer is converted to a native Java object.
+    final XrpOfferCreate offerCreate = XrpOfferCreate.from(offerCreateProto);
+
+    // THEN the OfferCreate converted as expected.
+    assertThat(offerCreate.takerGets())
+      .isEqualTo(XrpCurrencyAmount.from(offerCreateProto.getTakerGets().getValue()));
+    assertThat(offerCreate.takerPays())
+      .isEqualTo(XrpCurrencyAmount.from(offerCreateProto.getTakerPays().getValue()));
+
+    assertThat(offerCreate.expiration()).isEmpty();
+    assertThat(offerCreate.offerSequence()).isEmpty();
   }
 }
