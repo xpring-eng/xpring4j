f836655103bf4cff657a76986d51614bbb9ae915
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-19 11:56:30 -0700

    Fixes comments

--- src/test/java/io/xpring/xrpl/XrpEscrowFinishProtoConversionTest.java
+++ src/test/java/io/xpring/xrpl/XrpEscrowFinishProtoConversionTest.java
@@ -4,20 +4,19 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 import io.xpring.common.XrplNetwork;
 import io.xpring.xrpl.model.XrpEscrowFinish;
-import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.Test;
 import org.xrpl.rpc.v1.EscrowFinish;
 
 public class XrpEscrowFinishProtoConversionTest {
   @Test
   public void escrowFinishRequiredFieldsConversionTest() {
-    // GIVEN an EscrowCreate protocol buffer with required fields set.
+    // GIVEN an EscrowFinish protocol buffer with required fields set.
     final EscrowFinish escrowFinishProto = FakeXrpTransactionProtobufs.escrowFinishProtoWithRequiredFields;
 
     // WHEN the protocol buffer is converted to a native Java object.
     final XrpEscrowFinish escrowFinish = XrpEscrowFinish.from(escrowFinishProto, XrplNetwork.TEST);
 
-    // THEN the EscrowCreate converted as expected.
+    // THEN the EscrowFinish converted as expected.
     assertThat(escrowFinish.offerSequence()).isEqualTo(escrowFinishProto.getOfferSequence().getValue());
     ClassicAddress ownerClassicAddress = ImmutableClassicAddress.builder()
         .address(escrowFinishProto.getOwner().getValue().getAddress())
@@ -32,13 +31,13 @@ public class XrpEscrowFinishProtoConversionTest {
 
   @Test
   public void escrowFinishAllFieldsConversionTest() {
-    // GIVEN an EscrowCreate protocol buffer with all fields set.
+    // GIVEN an EscrowFinish protocol buffer with all fields set.
     final EscrowFinish escrowFinishProto = FakeXrpTransactionProtobufs.escrowFinishProtoWithAllFields;
 
     // WHEN the protocol buffer is converted to a native Java object.
     final XrpEscrowFinish escrowFinish = XrpEscrowFinish.from(escrowFinishProto, XrplNetwork.TEST);
 
-    // THEN the EscrowCreate converted as expected.
+    // THEN the EscrowFinish converted as expected.
     assertThat(escrowFinish.condition().get()).isEqualTo(escrowFinishProto.getCondition().getValue().toString());
     assertThat(escrowFinish.fulfillment().get()).isEqualTo(escrowFinishProto.getFulfillment().getValue().toString());
     assertThat(escrowFinish.offerSequence()).isEqualTo(escrowFinishProto.getOfferSequence().getValue());
@@ -52,7 +51,7 @@ public class XrpEscrowFinishProtoConversionTest {
 
   @Test
   public void escrowFinishMissingOfferSequenceTest() {
-    // GIVEN an EscrowCreate protocol buffer that's missing an offer sequence.
+    // GIVEN an EscrowFinish protocol buffer that's missing an offer sequence.
     final EscrowFinish escrowFinishProto = FakeXrpTransactionProtobufs.invalidEscrowFinishMissingOfferSequence;
 
     // WHEN the protocol buffer is converted to a native Java object.
@@ -64,7 +63,7 @@ public class XrpEscrowFinishProtoConversionTest {
 
   @Test
   public void escrowFinishMissingOwnerSequenceTest() {
-    // GIVEN an EscrowCreate protocol buffer that's missing an owner.
+    // GIVEN an EscrowFinish protocol buffer that's missing an owner.
     final EscrowFinish escrowFinishProto = FakeXrpTransactionProtobufs.invalidEscrowFinishMissingOwner;
 
     // WHEN the protocol buffer is converted to a native Java object.
