14a8de907ac97c72893f49e9bc7f1ca299a3ab06
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-19 12:14:31 -0700

    Adds docs for required fields

--- src/main/java/io/xpring/xrpl/model/XrpOfferCreate.java
+++ src/main/java/io/xpring/xrpl/model/XrpOfferCreate.java
@@ -4,6 +4,17 @@ import org.immutables.value.Value;
 
 import java.util.Optional;
 
+/**
+ * Represents an OfferCreate transaction on the XRP Ledger.
+ * <p>
+ * An OfferCreate transaction is effectively a limit order.
+ * It defines an intent to exchange currencies, and creates
+ * an Offer object if not completely fulfilled when placed.
+ * Offers can be partially fulfilled.
+ * </p>
+ *
+ * @see "https://xrpl.org/offercreate.html"
+ */
 @Value.Immutable
 public interface XrpOfferCreate {
   static ImmutableXrpOfferCreate.Builder builder() {
@@ -14,7 +25,19 @@ public interface XrpOfferCreate {
 
   Optional<Integer> offerSequence();
 
+  /**
+   * The amount and type of currency being provided by the offer creator.
+   *
+   * @return An {@link XrpCurrencyAmount} containing the amount and type of currency being provided by the offer
+   *         creator.
+   */
   XrpCurrencyAmount takerGets();
 
+  /**
+   * The amount and type of currency being requested by the offer creator.
+   *
+   * @return An {@link XrpCurrencyAmount} containing the amount and type of currency being requested by the offer
+   *         creator.
+   */
   XrpCurrencyAmount takerPays();
 }
