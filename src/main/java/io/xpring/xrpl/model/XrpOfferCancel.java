21d765b3bd9b618fb5fb431e9e04921cc9421143
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-19 11:42:52 -0700

    Adds docs

--- src/main/java/io/xpring/xrpl/model/XrpOfferCancel.java
+++ src/main/java/io/xpring/xrpl/model/XrpOfferCancel.java
@@ -1,4 +1,29 @@
 package io.xpring.xrpl.model;
 
+import org.immutables.value.Value;
+
+/**
+ * Represents an OfferCancel transaction on the XRP Ledger.
+ * <p>
+ * An OfferCancel transaction removes an Offer object from the XRP Ledger.
+ * </p>
+ *
+ * @see "https://xrpl.org/offercancel.html"
+ */
+@Value.Immutable
 public interface XrpOfferCancel {
+  static ImmutableXrpOfferCancel.Builder builder() {
+    return ImmutableXrpOfferCancel.builder();
+  }
+
+  /**
+   * The sequence number of a previous OfferCreate transaction.
+   * <p>
+   * If specified, cancel any offer object in the ledger that was created by that transaction.
+   * It is not considered an error if the offer specified does not exist.
+   * </p>
+   *
+   * @return A {@link Integer} containing the sequence number of a previous OfferCreate transaction.
+   */
+  public Integer offerSequence();
 }
