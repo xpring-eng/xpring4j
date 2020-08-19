ec206093fb6e96bf37ac45872a002fb351c89618
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-19 13:54:32 -0700

    Adds initial properties

--- src/main/java/io/xpring/xrpl/model/XrpPaymentChannelClaim.java
+++ src/main/java/io/xpring/xrpl/model/XrpPaymentChannelClaim.java
@@ -1,4 +1,31 @@
 package io.xpring.xrpl.model;
 
-public class XrpPaymentChannelClaim {
+import org.immutables.value.Value;
+
+import java.util.Optional;
+
+/**
+ * Represents a PaymentChannelClaim transaction on the XRP Ledger.
+ * <p>
+ * A PaymentChannelClaim transaction claims XRP from a payment channel, adjusts the payment channel's expiration,
+ * or both.
+ * </p>
+ *
+ * @see "https://xrpl.org/paymentchannelclaim.html"
+ */
+@Value.Immutable
+public interface XrpPaymentChannelClaim {
+  static ImmutableXrpPaymentChannelClaim.Builder builder() {
+    return ImmutableXrpPaymentChannelClaim.builder();
+  }
+
+  Optional<XrpCurrencyAmount> amount();
+
+  Optional<XrpCurrencyAmount> balance();
+
+  String channel();
+
+  Optional<String> publicKey();
+
+  Optional<String> signature();
 }
