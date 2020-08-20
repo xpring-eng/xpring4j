5701fc5d04312a64b7129ea7ff272a185f523f34
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-20 13:31:27 -0700

    Adds incomplete doc

--- src/main/java/io/xpring/xrpl/model/XrpPaymentChannelFund.java
+++ src/main/java/io/xpring/xrpl/model/XrpPaymentChannelFund.java
@@ -1,4 +1,17 @@
 package io.xpring.xrpl.model;
 
+import org.immutables.value.Value;
+
+/**
+ * Represents a PaymentChannelFund transaction on the XRP Ledger.
+ * <p>
+ * A PaymentChannelFund transaction adds additional XRP to an open payment channel, updates the expiration
+ * time of the channel, or both. Only the source address of the channel can use this transaction.
+ * (Transactions from other addresses fail with the error tecNO_PERMISSION.)
+ * </p>
+ *
+ * @see "https://xrpl.org/paymentchannelfund.html"
+ */
+@Value.Immutable
 public class XrpPaymentChannelFund {
 }
