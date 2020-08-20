5a0401791788ae230537805fb16793f88be8d7b5
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-20 11:06:49 -0700

    Adds doc

--- src/main/java/io/xpring/xrpl/model/XrpPaymentChannelCreate.java
+++ src/main/java/io/xpring/xrpl/model/XrpPaymentChannelCreate.java
@@ -1,4 +1,13 @@
 package io.xpring.xrpl.model;
 
+/**
+ * Represents a PaymentChannelCreate transaction on the XRP Ledger.
+ * <p>
+ * A PaymentChannelCreate transaction creates a unidirectional channel and funds it with XRP.
+ * The address sending this transaction becomes the "source address" of the payment channel.
+ * </p>
+ *
+ * @see "https://xrpl.org/paymentchannelcreate.html"
+ */
 public interface XrpPaymentChannelCreate {
 }
