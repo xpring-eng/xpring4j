7fcda1abb413da571b436df4c757a2ad9b0446a0
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-20 12:25:15 -0700

    Adds test for required fields

--- src/test/java/io/xpring/xrpl/XrpPaymentChannelCreateProtoConversionTest.java
+++ src/test/java/io/xpring/xrpl/XrpPaymentChannelCreateProtoConversionTest.java
@@ -1,4 +1,43 @@
 package io.xpring.xrpl;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import io.xpring.common.XrplNetwork;
+import io.xpring.xrpl.model.XrpCurrencyAmount;
+import io.xpring.xrpl.model.XrpPaymentChannelCreate;
+import org.junit.Test;
+import org.xrpl.rpc.v1.PaymentChannelCreate;
+
 public class XrpPaymentChannelCreateProtoConversionTest {
+  @Test
+  public void paymentChannelCreateRequiredFields() {
+    // GIVEN a PaymentChannelCreate protocol buffer with all fields set.
+    final PaymentChannelCreate paymentChannelCreateProto = FakeXrpTransactionProtobufs
+        .paymentChannelCreateWithRequiredFields;
+
+    // WHEN the protocol buffer is converted to a native Java object.
+    final XrpPaymentChannelCreate paymentChannelCreate = XrpPaymentChannelCreate
+        .from(paymentChannelCreateProto, XrplNetwork.TEST);
+
+    // THEN the PaymentChannelCreate converted as expected.
+    assertThat(paymentChannelCreate.amount())
+      .isEqualTo(XrpCurrencyAmount.from(paymentChannelCreateProto.getAmount().getValue()));
+
+    ClassicAddress destinationClassicAddress = ImmutableClassicAddress.builder()
+      .address(paymentChannelCreateProto.getDestination().getValue().getAddress())
+      .tag(paymentChannelCreateProto.getDestinationTag().getValue())
+      .isTest(true)
+      .build();
+    final String destinationXAddress = Utils.encodeXAddress(destinationClassicAddress);
+    assertThat(paymentChannelCreate.destinationXAddress()).isEqualTo(destinationXAddress);
+    
+    assertThat(paymentChannelCreate.publicKey())
+      .isEqualTo(paymentChannelCreateProto.getPublicKey().toString());
+    assertThat(paymentChannelCreate.settleDelay()).isEqualTo(paymentChannelCreateProto.getSettleDelay().getValue());
+
+    assertThat(paymentChannelCreate.cancelAfter()).isEmpty();
+  }
+
+  @Test
+  public void paymentChannelCreateAllFields() {}
 }
