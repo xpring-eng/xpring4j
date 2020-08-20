8509a0f9b02956a71bc9e7ffd8b54bd3434253fe
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-20 15:31:30 -0700

    Adds docs

--- src/main/java/io/xpring/xrpl/model/XrpSignerEntry.java
+++ src/main/java/io/xpring/xrpl/model/XrpSignerEntry.java
@@ -34,12 +34,26 @@ public interface XrpSignerEntry {
    */
   Integer signerWeight();
 
+  /**
+   * Constructs an {@link XrpSignerEntry} from a {@link Common.SignerEntry} protocol buffer.
+   *
+   * @param signerEntry A {@link Common.SignerEntry} (protobuf object) whose field values will be used to construct
+   *                    an {@link XrpSignerEntry}.
+   * @return An {@link XrpSignerEntry} with its fields set via the analogous protobuf fields.
+   * @see "https://github.com/ripple/rippled/blob/f43aeda49c5362dc83c66507cae2ec71cfa7bfdf/src/ripple/proto/org/xrpl/rpc/v1/common.proto#L471"
+   */
   static XrpSignerEntry from(Common.SignerEntry signerEntry) {
     if (!signerEntry.hasAccount() || !signerEntry.hasSignerWeight()) {
       return null;
     }
 
+    final String account = signerEntry.getAccount().toString();
+
+    final Integer signerWeight = signerEntry.getSignerWeight().getValue();
+
     return XrpSignerEntry.builder()
+        .account(account)
+        .signerWeight(signerWeight)
         .build();
   }
 }
