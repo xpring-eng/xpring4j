b664275d561542ede5fca820887cf831fcb41411
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-20 13:35:11 -0700

    Adds some docs

--- src/main/java/io/xpring/xrpl/model/XrpPaymentChannelFund.java
+++ src/main/java/io/xpring/xrpl/model/XrpPaymentChannelFund.java
@@ -22,7 +22,26 @@ public interface XrpPaymentChannelFund {
 
   XrpCurrencyAmount amount();
 
+  /**
+   * The unique ID of the channel to fund, as a 64-character hexadecimal string.
+   *
+   * @return A {@link String} containing the unique ID of the channel to fund, as a 64-character hexadecimal string.
+   */
   String channel();
 
+  /**
+   * (Optional) New Expiration time to set for the channel, in seconds since the Ripple Epoch.
+   * <p>
+   * This must be later than either the current time plus the SettleDelay of the channel,
+   * or the existing Expiration of the channel. After the Expiration time, any transaction
+   * that would access the channel closes the channel without taking its normal action.
+   * Any unspent XRP is returned to the source address when the channel closes.
+   * (Expiration is separate from the channel's immutable CancelAfter time.)
+   * For more information, see the PayChannel ledger object type: https://xrpl.org/paychannel.html
+   * </p>
+   *
+   * @return An {@link Integer} containing the new expiration time to set for the channel, in seconds since the
+   *         Ripple Epoch.
+   */
   Optional<Integer> expiration();
 }
