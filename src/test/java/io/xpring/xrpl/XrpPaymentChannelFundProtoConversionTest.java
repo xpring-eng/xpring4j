0754ce7e0699da2372466da80e809ea227f01361
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-20 13:47:24 -0700

    Adds empty tests

--- src/test/java/io/xpring/xrpl/XrpPaymentChannelFundProtoConversionTest.java
+++ src/test/java/io/xpring/xrpl/XrpPaymentChannelFundProtoConversionTest.java
@@ -1,4 +1,14 @@
 package io.xpring.xrpl;
 
+import org.junit.Test;
+
 public class XrpPaymentChannelFundProtoConversionTest {
+  @Test
+  public void paymentChannelFundRequiredFieldsTest() {}
+
+  @Test
+  public void paymentChannelFundAllFieldsTest() {}
+
+  @Test
+  public void paymentChannelFundMissingFieldsTest() {}
 }
