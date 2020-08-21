9868c88c64e35beb17c863384756fd6ea37a4d4d
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-21 14:57:51 -0700

    Adds blank tests

--- src/test/java/io/xpring/xrpl/XrpTrustSetProtoConversionTest.java
+++ src/test/java/io/xpring/xrpl/XrpTrustSetProtoConversionTest.java
@@ -3,4 +3,12 @@ package io.xpring.xrpl;
 import org.junit.Test;
 
 public class XrpTrustSetProtoConversionTest {
+  @Test
+  public void trustSetRequiredFieldsTest() {}
+
+  @Test
+  public void trustSetAllFieldsTest() {}
+
+  @Test
+  public void invalidTrustSetMissingLimitAmountTest() {}
 }
