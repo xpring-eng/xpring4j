4226051a98099b1ed146dcba5e5dd55de827fb19
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-21 10:44:37 -0700

    Adds doc

--- src/main/java/io/xpring/xrpl/model/XrpSignerListSet.java
+++ src/main/java/io/xpring/xrpl/model/XrpSignerListSet.java
@@ -1,4 +1,17 @@
 package io.xpring.xrpl.model;
 
+import org.immutables.value.Value;
+import org.xrpl.rpc.v1.SignerListSet;
+
+/**
+ * Represents a {@link SignerListSet} transaction on the XRP Ledger.
+ * <p>
+ * A {@link SignerListSet} transaction creates, replaces, or removes a list of signers that can be used to multi-sign a
+ * transaction. This transaction type was introduced by the MultiSign amendment.
+ * </p>
+ *
+ * @see "https://xrpl.org/signerlistset.html"
+ */
+@Value.Immutable
 public interface XrpSignerListSet {
 }
