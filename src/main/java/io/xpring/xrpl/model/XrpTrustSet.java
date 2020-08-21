956b6cd1b7fa1a6db566c202033c732aba79ccb7
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-21 14:23:55 -0700

    Adds doc

--- src/main/java/io/xpring/xrpl/model/XrpTrustSet.java
+++ src/main/java/io/xpring/xrpl/model/XrpTrustSet.java
@@ -1,4 +1,15 @@
 package io.xpring.xrpl.model;
 
+import org.immutables.value.Value;
+import org.xrpl.rpc.v1.TrustSet;
+
+/**
+ * Represents a {@link TrustSet} transaction on the XRP Ledger.
+ * <p>
+ * A {@link TrustSet} transaction creates or modifies a trust line linking two accounts.
+ * </p>
+ *
+ * @see "https://xrpl.org/trustset.html"
+ */
 public interface XrpTrustSet {
 }
