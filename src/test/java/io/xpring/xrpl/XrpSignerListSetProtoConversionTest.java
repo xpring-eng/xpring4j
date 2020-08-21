ec5284ffd43b0461319177a5cfab91417a6382cf
Ted Kalaw <ted.kalaw@gmail.com>
2020-08-21 13:14:38 -0700

    Adds empty valid tests

--- src/test/java/io/xpring/xrpl/XrpSignerListSetProtoConversionTest.java
+++ src/test/java/io/xpring/xrpl/XrpSignerListSetProtoConversionTest.java
@@ -1,4 +1,14 @@
 package io.xpring.xrpl;
 
+import org.junit.Test;
+
 public class XrpSignerListSetProtoConversionTest {
+  @Test
+  public void signerListSetWithSignerEntriesTest() {}
+
+  @Test
+  public void signerListSetWithNoSignerEntriesTest() {}
+
+  @Test
+  public void signerListSetWithSignerQuorum() {}
 }
